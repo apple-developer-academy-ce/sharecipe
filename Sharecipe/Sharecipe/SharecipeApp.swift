8f2a93c99e3e2f8dc468e6fc98556d8b74f379a3
Tales Valente <talesvalente@gmail.com>
2023-06-05 11:47:12 -0300

    First Deploy

---
+++ Sharecipe/Sharecipe/SharecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharecipeApp.swift
+//  Sharecipe
+//
+//  Created by Tales Valente on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SharecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
