c31a6759c61ca3d66f258c8d3e081aaf70ffdf56
Tales Valente <talesvalente@gmail.com>
2023-06-15 18:07:05 -0300

    AJUSTES FINAIS
    LOADINGVIEW

--- LoadingView.swift
+++ LoadingView.swift
@@ -3,21 +3,63 @@ import SwiftUI
 struct LoadingView: View {
     @State private var enableHomeView = false
 
+    init(){
+        for familyName in UIFont.familyNames {
+            print(familyName)
+
+            for fontName in UIFont.fontNames(forFamilyName: familyName) {
+                print ("\(fontName)")
+            }
+        }
+    }
+
     var body: some View {
         ZStack {
-            Color.green
+            // Use the AppLoadingScreenBG image as a tiling background
+            Image("AppBG")
+                .resizable()
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                Image("sharecipe") // Replace with your own logo image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200, alignment: .center)
+                .scaledToFill()
+                .opacity(0.2)
+
+            HStack {
+                VStack {
+                    Text("Flavory") // This is the title
+                        .font(.custom("HV-Cocktail-Regular", size: 32))
+                        .foregroundColor(Color.primary)
+                    Text("Your finest cooking app!") // This is the subtitle
+                        .font(.custom("HV-Cocktail-Regular", size: 16))
+                        .foregroundColor(Color.primary)
+
+                    Image("AppLoadingScreen") // Replace with your own logo image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200, alignment: .center)
+                }
+                .padding(10)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color(.systemGray6))
+            .opacity(0.7)
+            .overlay {
+                VStack {
+                    Text("Flavory") // This is the title
+                        .font(.custom("HV-Cocktail-Regular", size: 32))
+                        .foregroundColor(Color.primary)
+                    Text("Your finest cooking app!") // This is the subtitle
+                        .font(.custom("HV-Cocktail-Regular", size: 16))
+                        .foregroundColor(Color.primary)
+
+                    Image("AppLoadingScreen") // Replace with your own logo image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200, alignment: .center)
+                }
             }
 
         }
 
         .onAppear(perform: prepareToNavigate)
-
         .onTapGesture {
             self.enableHomeView = true
         }
@@ -30,5 +72,11 @@ struct LoadingView: View {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.enableHomeView = true
         }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
     }
 }
